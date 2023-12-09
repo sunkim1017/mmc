1937debce2fae3a81f60b7207ed78c7cf8de2b41
Haeoreum Kim <sunkim1017@team-everywhere.com>
2023-12-09 23:19:23 +0900

    버튼

--- src/component/check/CHplus.tsx
+++ src/component/check/CHplus.tsx
@@ -51,6 +51,36 @@ export default function CHplus() {
       </div>
       </div>
       
+      <div className="flex justify-start">
+      <Button
+          className="hover:bg-yellow-200 bg-yellow-100 hover:shadow-md"
+          size="large"
+          variant="outlined"
+          style={{ color: "#B45F04", borderColor: "#B45F04"}}
+          >
+        <Link href={"https://www.ebsi.co.kr/ebs/lms/player/retrieveLmsPlayerHtml5.ebs?sbjtapplyId=&sbjtId=S20230000448&lessonId=LS100030043035&lecGbn=V1M4"}>
+        <p className="font-extrabold text-xl">EBS 강의</p>
+        {/* <p className="font-extrabold text-xl">{`(ALGEOMATH)`}</p> */}
+        </Link>
+        </Button>
+        
+      </div>
+
+      <div className="flex justify-start">
+      <Button
+          className="hover:bg-yellow-200 bg-yellow-100 hover:shadow-md"
+          size="large"
+          variant="outlined"
+          style={{ color: "#B45F04", borderColor: "#B45F04"}}
+          >
+        <Link href={"https://www.youtube.com/watch?v=tlGVMth0yzk"}>
+        <p className="font-extrabold text-xl">유튜브 강의</p>
+        {/* <p className="font-extrabold text-xl">{`(ALGEOMATH)`}</p> */}
+        </Link>
+        </Button>
+        
+      </div>
+
       </div>
       </div>
 
